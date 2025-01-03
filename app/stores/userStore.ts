ba9c25bbe5324e4dd15a1f435e0b013bd9217362
niklhut <49069026+niklhut@users.noreply.github.com>
2025-01-03 22:46:13 +0100

    Add getters and actions

--- app/stores/userStore.ts
+++ app/stores/userStore.ts
@@ -6,11 +6,30 @@ export const useUserStore = defineStore("user", {
     user: null as UserDetail | null,
   }),
   actions: {
+    async loadUser() {
+      this.user = await $fetch("/api/users/me")
+    },
+    async logout() {
+      const { logout } = useEdgeDbIdentity()
+      await logout()
+      this.clearUser()
+    },
     setUser(user: UserDetail) {
       this.user = user
     },
     clearUser() {
       this.user = null
+    },
+  },
+  getters: {
+    isLoggedIn(): boolean {
+      return this.user !== null
+    },
+    isAdmin(): boolean {
+      return this.user?.role === "Admin"
+    },
+    isModerator(): boolean {
+      return this.user?.role === "Moderator"
     },
   },
 })
