f5c7f9029b7855afaa6b5966f01c235164832e3f
niklhut <49069026+niklhut@users.noreply.github.com>
2025-01-04 12:38:07 +0100

    Improve user store

--- app/stores/userStore.ts
+++ app/stores/userStore.ts
@@ -1,35 +1,54 @@
 import { defineStore } from "pinia"
 import type { UserDetail } from "#shared/types/User.type"
+import type { GetUserReturns } from "#edgedb/queries"
 
-export const useUserStore = defineStore("user", {
-  state: () => ({
-    user: null as UserDetail | null,
-  }),
-  actions: {
-    async loadUser() {
-      this.user = await $fetch("/api/users/me")
-    },
-    async logout() {
-      const { logout } = useEdgeDbIdentity()
-      await logout()
-      this.clearUser()
-    },
-    setUser(user: UserDetail) {
-      this.user = user
-    },
-    clearUser() {
-      this.user = null
-    },
-  },
-  getters: {
-    isLoggedIn(): boolean {
-      return this.user !== null
-    },
-    isAdmin(): boolean {
-      return this.user?.role === "Admin"
-    },
-    isModerator(): boolean {
-      return this.user?.role === "Moderator"
-    },
-  },
+export const useUserStore = defineStore("user", () => {
+  const user = ref<UserDetail | null>(null)
+  const edgedbIdentity = useEdgeDbIdentity()
+
+  async function loadUser() {
+    if (!import.meta.server) {
+      const loadedUser = await $fetch("/api/users/me")
+      user.value = loadedUser
+      return
+    } else if (edgedbIdentity.isLoggedIn.value) {
+      const userId = edgedbIdentity.identity.value.id
+      const loadedUser = await $fetch<GetUserReturns>(`/api/users/${userId}`)
+      if (loadedUser) {
+        user.value = { ...loadedUser, email: "" }
+      } else {
+        user.value = null
+      }
+    }
+  }
+
+  async function logout() {
+    console.log("Logging out")
+    await edgedbIdentity.logout()
+    clearUser()
+  }
+
+  function setUser(newUser: UserDetail) {
+    user.value = newUser
+  }
+
+  function clearUser() {
+    user.value = null
+  }
+
+  const isLoggedIn = edgedbIdentity.isLoggedIn
+  const isAdmin = computed(() => user.value?.role === "Admin")
+  const isModerator = computed(() => user.value?.role === "Moderator")
+
+  return {
+    user,
+    edgedbIdentity,
+    loadUser,
+    logout,
+    setUser,
+    clearUser,
+    isLoggedIn,
+    isAdmin,
+    isModerator,
+  }
 })
