1dd97e057938716a16b8ae6e59660cf7e969d7ff
niklhut <49069026+niklhut@users.noreply.github.com>
2025-02-03 20:36:30 +0100

    Convert link user endpoint to effect

--- server/api/users/link.post.ts
+++ server/api/users/link.post.ts
@@ -1,6 +1,17 @@
-export default defineEventHandler(async (req) => {
-  const { linkUserIdentity } = useEdgeDbQueries(req)
+import { Effect } from "effect"
 
-  const res = await linkUserIdentity()
-  return res
+export default defineEventHandler(async (req) => {
+  const handler = Effect.gen(function* () {
+    yield * Effect.logInfo("Linking user identity")
+
+    const user = yield * executeDatabaseQuery(req, "linkUserIdentity")
+    if (!user) {
+      return yield * new UnauthorizedError({ path: req.path })
+    }
+    yield * Effect.logInfo(`Linked user identity of ${user.id}`)
+
+    return user
+  })
+
+  return runPromiseAndCatch(handler, "link-user-identity")
 })
