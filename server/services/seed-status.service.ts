8fc01cd94d543eb94459b3a4204a30e4b70c5e40
niklhut <49069026+niklhut@users.noreply.github.com>
2025-04-05 20:27:30 +0200

    Formatting

--- server/services/seed-status.service.ts
+++ server/services/seed-status.service.ts
@@ -1,16 +1,19 @@
 import { Effect } from "effect"
 import { SeedStatusRepository, SeedStatusRepositoryLayer } from "../repositories/seed-status.repository"
 
-export class SeedStatusService extends Effect.Service<SeedStatusService>()("app/services/SeedStatusService", {
-  effect: Effect.gen(function* () {
-    const repo = yield* SeedStatusRepository
+export class SeedStatusService extends Effect.Service<SeedStatusService>()(
+  "app/services/SeedStatusService",
+  {
+    effect: Effect.gen(function* () {
+      const repo = yield* SeedStatusRepository
 
-    const checkSeedExists = (seedName: string) => repo.checkSeedExists(seedName)
-    const createSeedRecord = (seedName: string) => repo.createSeedRecord(seedName)
+      const checkSeedExists = (seedName: string) => repo.checkSeedExists(seedName)
+      const createSeedRecord = (seedName: string) => repo.createSeedRecord(seedName)
 
-    return { checkSeedExists, createSeedRecord } as const
-  }),
-  dependencies: [SeedStatusRepositoryLayer],
-}) { }
+      return { checkSeedExists, createSeedRecord } as const
+    }),
+    dependencies: [SeedStatusRepositoryLayer],
+  },
+) { }
 
 export const SeedStatusServiceLayer = SeedStatusService.DefaultWithoutDependencies
