76b427b46b5170db3e4e12d391b8b2f9c0ddabbc
niklhut <49069026+niklhut@users.noreply.github.com>
2025-04-19 22:07:51 +0200

    Respect login redirect query in middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -11,6 +11,10 @@ export default defineNuxtRouteMiddleware(async (to, _from) => {
   await userStore.loadUser()
   // Redirect to profile if user is logged in and trying to access login
   if (to.fullPath.startsWith("/login") && userStore.loggedIn) {
+    const redirect = to.query.redirect as string | undefined
+    if (redirect) {
+      return navigateTo(redirect)
+    }
     return navigateTo("/profile")
   }
   // Redirect to login if user is not logged in and trying to access protected route
